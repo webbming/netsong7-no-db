9a22f655a9fc068b92d76cbd0298484dd2aac390
netsong7 <netsong7@daum.net>
2025-03-30 00:46:15 +0900

    Update IndexController.java

--- src/main/java/com/example/IndexController.java
+++ src/main/java/com/example/IndexController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
